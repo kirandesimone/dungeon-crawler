a30f3cd12174989d578235b06c98a549e1560589
kirandesimone <kirandesimone@gmail.com>
2022-01-08 17:21:58 -0500

    Stacking Enemies bug fixed

--- src/systems/movement.rs
+++ src/systems/movement.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
+use std::collections::HashSet;
 
 #[system(for_each)]
 #[read_component(Player)]
 #[read_component(FieldOfView)]
+#[read_component(Point)]
 pub fn movement(
     entity: &Entity,
     wants_move: &WantsToMove,
@@ -11,6 +13,13 @@ pub fn movement(
     sub_world: &mut SubWorld,
     commands: &mut CommandBuffer,
 ) {
+    let mut points = <(Entity, &Point)>::query();
+    let entities_pos: HashSet<&Point> = points
+        .iter(sub_world)
+        .filter(|(entity, _)| **entity != wants_move.entity)
+        .map(|(_, pos)| pos)
+        .collect();
+
     if let Ok(entry) = sub_world.entry_ref(wants_move.entity) {
         if let Ok(fov) = entry.get_component::<FieldOfView>() {
             commands.add_component(wants_move.entity, fov.clone_dirty());
@@ -24,17 +33,9 @@ pub fn movement(
         }
     }
 
-    if map.can_enter_tile(wants_move.destination) {
+    if map.can_enter_tile(wants_move.destination) && !entities_pos.contains(&wants_move.destination)
+    {
         commands.add_component(wants_move.entity, wants_move.destination);
-        // Checking if the entity is the Player
-        if sub_world
-            .entry_ref(wants_move.entity)
-            .unwrap()
-            .get_component::<Player>()
-            .is_ok()
-        {
-            camera.on_player_move(wants_move.destination);
-        }
     }
     commands.remove(*entity);
 }
